02a281da3121a5f7730c0f4ee418ee0fc5db3686
zmendless <105993706+zmendless@users.noreply.github.com>
2025-10-24 17:55:04 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'mark-tracker-v1';
+const urlsToCache = [
+  'index.html',
+  'manifest.json',
+  'students.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
